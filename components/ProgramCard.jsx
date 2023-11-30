1e4af404cc3d743a65942e6fa9225a1f2b39b087
Malte Jensen <malt656c@stud.kea.dk>
2023-11-30 09:44:32 +0100

    awfawf

--- components/ProgramCard.jsx
+++ components/ProgramCard.jsx
@@ -1,15 +1,8 @@
 import styles from "./ProgramCard.module.css";
 export default async function ProgramCard(props) {
-  console.log(props.act)
     return (
       <article className={styles.card}>
-        <img src="" alt="" />
-        <p>{props.act}</p>
-        <div className={styles.time}>
-          <span>{props.start}</span>
-          <span>-</span>
-          <span>{props.end}</span>
-        </div>
+
       </article>
     );
   }
--- components/ProgramVenue.jsx
+++ components/ProgramVenue.jsx
@@ -1,15 +1,9 @@
 import ProgramCard from "./ProgramCard";
 import styles from "./ProgramVenue.module.css";
-export default function ProgramVenue(props) {
-  console.log(props.scheduleForDay);
-  return (
+export default async function ProgramVenue(props) {
+ return  (
     <section>
-      <h2>{props.venue}</h2>
-      <div>
-        {props.scheduleForDay.map((i) => {
-          <ProgramCard start={i.start} end={i.end} act={i.act}></ProgramCard>;
-        })}
-      </div>
+
     </section>
   );
 }
