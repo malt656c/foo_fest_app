f832f91720df501b33e53f61287f9da7c302fb8f
magnuslykke <magn965m@stud.kea.dk>
2023-11-30 10:04:55 +0100

    Footer

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css";
+export default function Footer() {
+  return (
+    <section className={styles.footerBackground}>
+      <h3>Få Hjælp</h3>
+      <p>kontakt</p>
+      <p>bla bla bla</p>
+    </section>
+  );
+}
