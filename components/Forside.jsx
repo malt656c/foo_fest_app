9017776c8d8dd7237bfcef9a4a42571a2bcabcb4
magnuslykke <magn965m@stud.kea.dk>
2023-11-30 09:55:34 +0100

    menu ting

--- components/Forside.jsx
+++ components/Forside.jsx
@@ -2,8 +2,12 @@ import styles from "./Forside.module.css";
 export default function Forside() {
   return (
     <section className={styles.forsideWrapper}>
+      <a href="/menu"> SE den dumme menu</a>
+
       <h1 className={styles.forsideHeader}>FooFest</h1>
-      <button className={styles.forsideButton}><a href="/program">Se Program</a></button>
+      <button className={styles.forsideButton}>
+        <a href="/program">Se Program</a>
+      </button>
     </section>
   );
 }
--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,4 +1,9 @@
-import styles from "./Menu.module.css"
-export default function Menu(){
-    return <section></section>
+import styles from "./Menu.module.css";
+export default function Menu() {
+  return (
+    <section>
+      <a href="/">FOOFEST</a>
+      <a href="/program">Menu prøve prunt for program</a>
+    </section>
+  );
 }
