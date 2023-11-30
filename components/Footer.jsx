8b95aa2339f067b739b274ac196f4a98d7b0cf68
magnuslykke <magn965m@stud.kea.dk>
2023-11-30 11:22:03 +0100

    foot foot

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,27 +2,38 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <section className={styles.footerBackground}>
-      <div>
-        <h3>Få Hjælp</h3>
-        <p>Ofte stillede sprøgmsål</p>
-        <p>Kontakt</p>
-
-        <p>Fortrydelse & returnering</p>
-        <p>Handelsbetingelser</p>
-        <p>persondatapolitik</p>
+      <div className={styles.footerGapGrid}>
+        <div>
+          <h3 className={styles.footerH3}>Få Hjælp</h3>
+        </div>
+        <div>
+          <p>Ofte stillede sprøgmsål</p>
+          <p>Kontakt</p>
+        </div>
+        <div>
+          <p>Fortrydelse & returnering</p>
+          <p>Handelsbetingelser</p>
+          <p>persondatapolitik</p>
+        </div>
       </div>
-      <div>
-        <h3>Om foofest</h3>
-        <p>Job og Karriere</p>
-        <p>bliv frivillig</p>
-        <p>Samarbejdspartnere</p>
+      <div className={styles.footerGapGrid}>
+        <div>
+          <h3 className={styles.footerH3}>Om foofest</h3>
+        </div>
+        <div>
+          <p>Job og Karriere</p>
+          <p>bliv frivillig</p>
+          <p>Samarbejdspartnere</p>
+        </div>
       </div>
-      <div>
-        <h3>Følg os på de sociale medier</h3>
+      <div className={styles.footerGapGrid}>
+        <div>
+          <h3 className={styles.footerH3}>Følg os på de sociale medier</h3>
+        </div>
         <div className={styles.footerIcons}>
-          <img src="/facebook-f.svg" alt="facebook" />
-          <img src="/instagram(2).svg" alt="instagram" />
-          <img src="/tiktok.svg" alt="tiktok" />
+          <img className={styles.footerImg} src="/facebook-f.svg" alt="facebook" />
+          <img className={styles.footerImg} src="/instagram(2).svg" alt="instagram" />
+          <img className={styles.footerImg} src="/tiktok.svg" alt="tiktok" />
         </div>
       </div>
     </section>
