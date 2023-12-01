cd33db9eb3cd6be19f8d93e77efc970c27b37ad6
Luka Andersen <luka_andersen@hotmail.com>
2023-12-01 13:22:34 +0100

    Luka - part 2

--- components/Bookingcomplete.jsx
+++ components/Bookingcomplete.jsx
@@ -1,1 +1,13 @@
+import styles from "./BookingComplete.module.css";
+export default function BookingComplete() {
+    return (
+        <div className={styles.CompleteCard}>
+        <h1 className={styles.Heading}>Tak for dit køb!</h1>
+        <p className={styles.Text}>Vi har sendt en ordrebekræftelselse til e-mailen: xxx@gmail.com. 
 
+        Skulle du have yderligere spørgsmål er du velkommen til at kontakte FooFest på kontakt-foofest@gmail.com</p>
+       
+        <button className={styles.Button}>Go to Frontpage</button>
+        </div>
+    )
+}
---
+++ src/app/ordercomplete/page.jsx
@@ -0,0 +1,11 @@
+import AppWrapper from "../../../components/AppWrapper";
+import BookingComplete from "../../../components/BookingComplete";
+export default function Home() {
+  return (
+    <main>
+      <AppWrapper>
+<BookingComplete></BookingComplete>
+      </AppWrapper>
+  </main>
+  );
+}
