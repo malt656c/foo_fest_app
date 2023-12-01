506afba68f96b28c293827681e508ab706917062
magnuslykke <magn965m@stud.kea.dk>
2023-12-01 16:02:27 +0100

    forside og menu

--- components/Forside.jsx
+++ components/Forside.jsx
@@ -1,13 +1,56 @@
+// import styles from "./Forside.module.css";
+// import KoncertCard from "./KoncertCard";
+// export default function Forside() {
+//   return (
+//     <section className={styles.forsideWrapper}>
+//       <div className={styles.forsideBilledeContainer}>
+//         <img className={styles.forsideBillede} src="/frontpagebg.jpg" alt="koncert billede på forsiden" />
+//         <div className={styles.forsideContent}>
+//           <h1 className={styles.forsideHeader}>FooFest</h1>
+//           <button className={styles.forsideButton}>
+//             <a href="/program">Buy tickets</a>
+//           </button>
+//         </div>
+//       </div>
+//       <div>Her skal banneret ind</div>
+//       <div>
+//         <h2>13 New Bands Booked for Foofest</h2>
+//         <div>
+//           <KoncertCard></KoncertCard>
+//           <KoncertCard></KoncertCard>
+//           <KoncertCard></KoncertCard>
+//         </div>
+//         <button className={styles.forsideButton}>
+//           <a href="/program">Fuldt program</a>
+//         </button>
+//       </div>
+//     </section>
+//   );
+// }
 import styles from "./Forside.module.css";
+import KoncertCard from "./KoncertCard";
 export default function Forside() {
   return (
     <section className={styles.forsideWrapper}>
-      <a href="/sletmigmenu"> SE den dumme menu</a>
+      <div className={styles.forsideContent}>
+        <h1 className={styles.forsideHeader}>FooFest</h1>
+        <button className={styles.forsideButton}>
+          <a href="/program">Buy tickets</a>
+        </button>
+      </div>
 
-      <h1 className={styles.forsideHeader}>FooFest</h1>
-      <button className={styles.forsideButton}>
-        <a href="/program">Se Program</a>
-      </button>
+      <div className={styles.forsideBanner}>Her skal banneret ind</div>
+      <div className={styles.forsideContentBottom}>
+        <h2>13 New Bands Booked for Foofest</h2>
+        <div>
+          <div className={styles.forsideArrowLeft}></div>
+          <img src="/placeholdecard.png" alt="placeholdecard" />
+        </div>
+        <div className={styles.forsideArrowRight}></div>
+        <button className={styles.forsideBottomButton}>
+          <a href="/program">Fuldt program</a>
+        </button>
+      </div>
     </section>
   );
 }
--- components/Menu.jsx
+++ components/Menu.jsx
@@ -4,16 +4,28 @@ export default function Menu() {
   return (
     <section className={styles.menuBackground}>
       <h2 className={styles.menuH2}>
-        Program <span className={styles.menuH2span1}>program</span> <span className={styles.menuH2span2}>program</span>
+        Program <span className={styles.menuH2span1}>program</span>
       </h2>
-      <h2 className={styles.menuH2}>Billetter</h2>
-      <h2 className={styles.menuH2}>Nyheder</h2>
-      <h2 className={styles.menuH2}>Bliv Frivillig</h2>
-      <h2 className={styles.menuH2}>Om Foofest</h2>
-      <h2 className={styles.menuH2}>Kontakt</h2>
+      <h2 className={styles.menuH2}>
+        Billetter <span className={styles.menuH2span1}>billetter</span>
+      </h2>
+      <h2 className={styles.menuH2}>
+        Nyheder <span className={styles.menuH2span1}>nyheder</span>
+      </h2>
+      <h2 className={styles.menuH2}>
+        Bliv Frivillig<span className={styles.menuH2span1}>bliv frivillig</span>
+      </h2>
+      <h2 className={styles.menuH2}>
+        Om Foofest <span className={styles.menuH2span1}>om foofest</span>
+      </h2>
+      <h2 className={styles.menuH2}>
+        Kontakt<span className={styles.menuH2span1}>kontakt</span>
+      </h2>
     </section>
   );
 }
 
 //Translate menu 100 % så den er ude af skærmen.
 //Når der klikkes på den skal den translate til 0, så den så fylder hele skærmen (vh)
+
+/* <span className={styles.menuH2span1}>program</span>; */
