b7106fde1d16a9ab4089fecfeadb0975edd905a8
magnuslykke <magn965m@stud.kea.dk>
2023-12-04 11:35:28 +0100

    checkout

--- components/Checkout.jsx
+++ components/Checkout.jsx
@@ -4,20 +4,71 @@ export default function Checkout() {
     <section className={styles.checkoutWrapper}>
       <div className={styles.checkoutOplysninger}>
         <h1>Betalingsoplysninger</h1>
-        <form className={styles.checkoutFormFlex}>
-          <label for="firstName">Fornavn:</label>
-          <input type="text" id="firstName" name="firstName" placeholder="Poul-Erik" required></input>
+        <form className={styles.checkoutFormGrid}>
+          <label className={styles.checkoutLabel} for="firstName">
+            Fornavn
+          </label>
+          <input className={styles.checkoutInput} type="text" id="firstName" name="firstName" placeholder="Poul-Erik" required></input>
 
-          <label for="lastName">Efternavn:</label>
-          <input type="text" id="lastName" name="lastName" required></input>
+          <label className={styles.checkoutLabel} for="lastName">
+            Efternavn
+          </label>
+          <input className={styles.checkoutInput} type="text" id="lastName" name="lastName" required></input>
 
-          <label htmlFor="Adresse">Adresse</label>
-          <input type="text" name="Adresse" id="adresse" required />
+          <label className={styles.checkoutLabel} htmlFor="Adresse">
+            Adresse
+          </label>
+          <input className={styles.checkoutInput} type="text" name="Adresse" id="adresse" required />
 
-          <label for="email">Email:</label>
-          <input type="email" id="email" name="email" required></input>
+          <div className={styles.checkoutEtage}>
+            <div className={styles.checkoutDoor}>
+              <label className={styles.checkoutLabel} htmlFor="etage">
+                Etage
+              </label>
+              <input className={styles.checkoutInput} type="text" name="etage" id="etage" />
+            </div>
+            <div className={styles.checkoutDoor}>
+              <label className={styles.checkoutLabel} htmlFor="door">
+                Door
+              </label>
+              <input className={styles.checkoutInput} type="text" name="door" id="door" />
+            </div>
+          </div>
 
-          <button>send</button>
+          <label className={styles.checkoutLabel} htmlFor="postnummer">
+            Zip code
+          </label>
+          <input className={styles.checkoutInput} type="number" name="zipcode" id="zipcode" />
+
+          <label className={styles.checkoutLabel} htmlFor="city">
+            City
+          </label>
+          <input className={styles.checkoutInput} type="text" name="city" id="city" />
+
+          <label className={styles.checkoutLabel} for="email">
+            Email:
+          </label>
+          <input className={styles.checkoutInput} type="email" id="email" name="email" required></input>
+
+          <label className={styles.checkoutLabel} htmlFor="telephonenumber">
+            Telephone
+          </label>
+          <input className={styles.checkoutInput} type="number" name="telephonenumber" id="telephonenumber" />
+
+          <label className={styles.checkoutLabel} htmlFor="kommentar">
+            Comments
+          </label>
+          <input className={styles.checkoutInput} type="text" name="kommentar" id="kommentar" />
+
+          <label className={styles.checkoutLabel} htmlFor="betalingsmetode">
+            Payment method
+          </label>
+          <div className={styles.checkoutButtonGrid}>
+            <button>Card/visa</button>
+            <button>Mobilepay</button>
+            <button>Gavekort</button>
+            <button>Viabill</button>
+          </div>
         </form>
       </div>
       <div>
