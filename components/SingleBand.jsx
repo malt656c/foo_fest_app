c3dbafad193fc4c20e88665095fc17f00bbcc7b2
Malte Jensen <malt656c@stud.kea.dk>
2023-12-04 15:12:39 +0100

    single band side

--- components/SingleBand.jsx
+++ components/SingleBand.jsx
@@ -1,5 +1,40 @@
-'use client'
-import styles from "./SingleBand.module.css"
-export default function SingleBand(){
-    return <section></section>
+"use client";
+import Image from "next/image";
+import styles from "./SingleBand.module.css";
+export default function SingleBand(props) {
+  const url = "http://localhost:8080/logos/";
+  const data = props.content;
+  console.log(data);
+  const imageSource = () => {
+    if (data.logoCredits || !data.logo.includes("http")) {
+      return (
+        <div className={styles.imageWrapper}>
+          <Image src={url + data.logo} alt={data.name} height={350} width={350} className={styles.bandImage} />
+          <span className={styles.logoCredits}>{data.logoCredits}</span>
+        </div>
+      );
+    } else {
+      return <Image src={data.logo} alt={data.name} height={350} width={350} className={styles.bandImage} />;
+    }
+  };
+  return (
+    <section className={styles.wrapper}>
+      <article className={styles.bandContent}>
+        {imageSource()}
+        <div className={styles.bandInfo}>
+          <h1>{data.name}</h1>
+          <div className={styles.bandGeneral}>
+            <h2>Genre: {data.genre}</h2>
+            <div className={styles.bandMembers}>
+              <h3>Members:</h3>
+              {data.members.map((member) => {
+                return <span key={member}>{member}</span>;
+              })}
+            </div>
+          </div>
+          <p>{data.bio}</p>
+        </div>
+      </article>
+    </section>
+  );
 }
--- src/app/band/[slug]/page.jsx
+++ src/app/band/[slug]/page.jsx
@@ -1,4 +1,7 @@
-import SingleBand from "../../../../components/SingleBand"
-export default function Page({ params }) {
-    return <div>{params.slug}</div>
-  }
+import SingleBand from "../../../../components/SingleBand";
+import { FFGet } from "../../../../components/utilities";
+export default async function Page({ params }) {
+  const data = await FFGet(`bands/${params.slug}`);
+  console.log(await data)
+  return <SingleBand content={data}></SingleBand>;
+}
