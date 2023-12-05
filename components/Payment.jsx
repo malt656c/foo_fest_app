527a888951c2143c45fdf9f4c9e83b4aafeafc93
magnuslykke <magn965m@stud.kea.dk>
2023-12-05 11:16:13 +0100

    payment

--- components/Payment.jsx
+++ components/Payment.jsx
@@ -12,27 +12,29 @@ export default function Payment() {
           <label className={styles.paymentLabel} htmlFor="navn">
             Navn På Kortet
           </label>
-          <input className={styles.paymentInput} type="text" name="navn" id="navn" />
+          <input className={styles.paymentInput} type="text" name="navn" id="navn" required />
           <label className={styles.paymentLabel} htmlFor="kortnummer">
             Card Number
           </label>
-          <input className={styles.paymentInput} type="number" name="kortnummer" id="kortnummer" />
+          <input className={styles.paymentInput} type="number" name="kortnummer" id="kortnummer" required />
           <div className={styles.paymentExpire}>
             <div className={styles.paymentCVC}>
               <label className={styles.paymentLabel} htmlFor="expiredate">
                 Expire Date
               </label>
-              <input className={styles.paymentInput} type="text" name="expiredate" id="expiredate" pattern="\d{2}/\d{2}" placeholder="xx/xx" />
+              <input className={styles.paymentInput} type="text" name="expiredate" id="expiredate" pattern="\d{2}/\d{2}" placeholder="xx/xx" required />
             </div>
             <div className={styles.paymentCVC}>
               <label className={styles.paymentLabel} htmlFor="CVC">
                 CVC
               </label>
-              <input className={styles.paymentInput} type="number" name="CVC" id="CVC" />
+              <input className={styles.paymentInput} type="number" min={0} max={999} name="CVC" id="CVC" required />
             </div>
           </div>
         </form>
-        <button className={styles.paymentConfirmButton}>Confirm Payment</button>
+        <a href="/ordercomplete">
+          <button className={styles.paymentConfirmButton}>Confirm Payment</button>
+        </a>
       </div>
     </section>
   );
