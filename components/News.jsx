0ce3d1b6705f9775ea99579ddb1a1626781f060c
Malte Jensen <malt656c@stud.kea.dk>
2023-12-06 14:09:09 +0100

    asd

--- components/News.jsx
+++ components/News.jsx
@@ -2,12 +2,10 @@
 import { useEffect, useState } from "react";
 import styles from "./News.module.css";
 import { FFGet } from "./utilities";
-import { Divide } from "hamburger-react";
 export default function News() {
   const [events, setEvents] = useState(null);
   useEffect(() => {
     async function getEvents() {
-        
       if (events === null) {
         const ffevents=await FFGet("events")
         if(ffevents.length!==0)
