d30fb69ed3efd8a49914754acfcf7769dc603765
Luka Andersen <luka_andersen@hotmail.com>
2023-12-08 10:26:33 +0100

    cart part 2

--- components/Header.jsx
+++ components/Header.jsx
@@ -13,19 +13,16 @@ export default function Header() {
   //HIV STATE IND DEL 2
   const productsInCart = useContext(StateContext);
   return (
-    <section className={styles.headerBackground}>
-      <Link href={"/"}>
-        <Image src="/mobilelogo.svg" alt="logo" height={48} width={48} />
-      </Link>
-      <div className={styles.CartAndMenu}>
-        <Shoppingcart visibility={cartVisibility} products={productsInCart}></Shoppingcart>
-        <button
-          className={styles.cartIcon}
-          onClick={() => {
-            cartVisibility ? setCartVisible(false) : setCartVisible(true);
-          }}
-        >
-          <Image src="/cart-shopping-solid.svg" alt="Cart" width={50} height={50}></Image>
+      <section className={styles.headerBackground}>
+        <Link href={"/"}>
+          <Image src="/mobilelogo.svg" alt="logo" height={48} width={48} />
+        </Link>
+        <div className={styles.CartAndMenu}>
+        <Shoppingcart
+        visibility={cartVisibility}
+        products={productsInCart}></Shoppingcart>
+        <button className={styles.cartIcon}  onClick={() => setCartVisible(true)}> 
+        <Image src="/cart-shopping-solid.svg" alt="Cart" width={70} height={70}></Image>
         </button>
         <Menu></Menu>
       </div>
