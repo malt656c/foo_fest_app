8a126d1d617b0d619fc491c5aa39c6568a054a5b
Malte Jensen <malt656c@stud.kea.dk>
2023-12-12 19:22:30 +0100

    cvzfbf

--- components/Checkout.jsx
+++ components/Checkout.jsx
@@ -7,53 +7,54 @@ import UsersOrder from "./UsersOrder";
 import CheckoutInput from "./CheckoutInput";
 export default function Checkout() {
   const { userInfo, setUserInfo, productsInCart, setProductsInCart } = useContext(dataContext);
-  const router = useRouter()
-  const SubmitEvent =(event)=>{
+  const router = useRouter();
+  const SubmitEvent = (event) => {
     event.preventDefault();
-    const formData=new FormData(event.target)
-    const data={...Object.fromEntries(formData),productsBought:[...productsInCart]}
-   delete data.myCheckbox
-    setUserInfo(data)
-router.push("/payment")
-  }
+    const formData = new FormData(event.target);
+    const data = { ...Object.fromEntries(formData), productsBought: [...productsInCart] };
+    delete data.myCheckbox;
+    setUserInfo(data);
+    router.push("/payment");
+  };
   return (
     <section className={styles.checkoutWrapper}>
-            <UsersOrder></UsersOrder>
-      <h2>Personal information</h2>
       <form className={styles.checkoutForm} onSubmit={SubmitEvent}>
-        <CheckoutInput name="firstName" label="First Name" required />
-        <CheckoutInput name="lastName" label="Last Name" required />
-        <CheckoutInput name="address" label="Address" required />
-        <div className={styles.checkoutEtage}>
-          <CheckoutInput name="floor" label="Floor" />
-          <CheckoutInput name="door" label="Door" />
-        </div>
-        <CheckoutInput type="number" name="zipCode" label="Zipcode" required />
-        <CheckoutInput name="city" label="City" required />
-        <CheckoutInput type="email" name="email" label="Email" required />
-        <CheckoutInput type="tel" name="phone" label="Phone Number" required />
-        <CheckoutInput name="comments" label="Comments" />
+        <UsersOrder></UsersOrder>
+        <div className={styles.inputWrapper}>
+          <h2>Personal information</h2>
+          <CheckoutInput name="firstName" label="First Name" required />
+          <CheckoutInput name="lastName" label="Last Name" required />
+          <CheckoutInput name="address" label="Address" required />
+          <div className={styles.checkoutEtage}>
+            <CheckoutInput name="floor" label="Floor" />
+            <CheckoutInput name="door" label="Door" />
+          </div>
+          <CheckoutInput type="number" name="zipCode" label="Zipcode" required />
+          <CheckoutInput name="city" label="City" required />
+          <CheckoutInput type="email" name="email" label="Email" required />
+          <CheckoutInput type="tel" name="phone" label="Phone Number" required />
+          <CheckoutInput name="comments" label="Comments" />
 
-        <div className={styles.paymentOptions}>
-          <h3>Payment Method</h3>
-          <div>
-            <label htmlFor="card">Card</label>
-            <input type="radio" name="paymentMethod" id="card" value="card" />
-          </div>
-          <div>
-            <label htmlFor="mobilepay">MobilePay</label>
-            <input type="radio" name="paymentMethod" id="mobilepay" value="mobilepay" />
-          </div>
-          <div>
-            <label htmlFor="giftcard">Giftcard</label>
-            <input type="radio" name="paymentMethod" id="giftcard" value="giftcard" />
-          </div>
-          <div>
-            <label htmlFor="viabill">Viabill</label>
-            <input type="radio" name="paymentMethod" id="viabill" value="viabill" />
+          <div className={styles.paymentOptions}>
+            <h3>Payment Method</h3>
+            <div>
+              <label htmlFor="card">Card</label>
+              <input type="radio" name="paymentMethod" id="card" value="card" />
+            </div>
+            <div>
+              <label htmlFor="mobilepay">MobilePay</label>
+              <input type="radio" name="paymentMethod" id="mobilepay" value="mobilepay" />
+            </div>
+            <div>
+              <label htmlFor="giftcard">Giftcard</label>
+              <input type="radio" name="paymentMethod" id="giftcard" value="giftcard" />
+            </div>
+            <div>
+              <label htmlFor="viabill">Viabill</label>
+              <input type="radio" name="paymentMethod" id="viabill" value="viabill" />
+            </div>
           </div>
         </div>
-
         <div className={styles.termsWrapper}>
           <input type="text" name="promoCode" placeholder="Enter Promocode" />
 
@@ -62,11 +63,10 @@ router.push("/payment")
             <label htmlFor="myCheckbox">Jeg accepterer handelsbetingelserne og giver samtykke til at Foo-Fest behandler mine persondata.</label>
           </div>
         </div>
-          <button role="button" className={styles.checkoutButton}>
-            Confirm order
-          </button>
+        <button role="button" className={styles.checkoutButton}>
+          Confirm order
+        </button>
       </form>
-
     </section>
   );
 }
