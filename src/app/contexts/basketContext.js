9d5bac2dce8cc18607c259a573f9907a598a0a05
Malte Jensen <malt656c@stud.kea.dk>
2023-12-12 11:45:13 +0100

    GDA

--- src/app/contexts/basketContext.js
+++ src/app/contexts/basketContext.js
@@ -1,20 +1,10 @@
 "use client";
-//contexts/numberContext.js
 import { createContext, useState } from "react";
-
-/* export const StateContext = createContext([]);
-export const UpdaterContext = createContext(null); */
 export const dataContext = createContext({ data: { userInfo: null, setUserInfo: () => {}, productsInCart: null, setProductsInCart: () => {} } });
 
 export const BasketProvider = ({ children }) => {
   const [productsInCart, setProductsInCart] = useState([]);
   const [userInfo, setUserInfo] = useState([]);
   console.log(userInfo);
-  return (
-    /*     <StateContext.Provider value={productsInCart}>
-      <UpdaterContext.Provider value={setProductsInCart}> */
-    <dataContext.Provider value={{ userInfo, setUserInfo, productsInCart, setProductsInCart }}>{children}</dataContext.Provider>
-    /*       </UpdaterContext.Provider>
-    </StateContext.Provider> */
-  );
+  return <dataContext.Provider value={{ userInfo, setUserInfo, productsInCart, setProductsInCart }}>{children}</dataContext.Provider>;
 };
