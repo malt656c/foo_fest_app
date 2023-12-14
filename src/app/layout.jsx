ab7987bccc98f2565e990f8fca1f1bb3dfdcc6ae
magnuslykke <magn965m@stud.kea.dk>
2023-12-14 10:26:40 +0100

    favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import {BasketProvider} from "./contexts/basketContext"
+import { BasketProvider } from "./contexts/basketContext";
 import { Suspense } from "react";
 import Loading from "./loading";
 const inter = Inter({ subsets: ["latin"] });
@@ -16,14 +16,16 @@ const inter = Inter({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/mobilelogo.svg" sizes="any" />
+      </head>
       <body className={inter.className}>
         <BasketProvider>
-          
-        <header>
-          <Header></Header>
-        </header>
-        <Suspense fallback={<Loading/>}>{children}</Suspense>
-        <Footer></Footer>
+          <header>
+            <Header></Header>
+          </header>
+          <Suspense fallback={<Loading />}>{children}</Suspense>
+          <Footer></Footer>
         </BasketProvider>
       </body>
     </html>
